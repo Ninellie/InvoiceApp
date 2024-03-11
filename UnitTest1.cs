c949f457a57affbe92155d9297f801975d0ff64f
Ninellie <a.pawlow2009@ya.ru>
2024-03-11 19:58:00 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+using NPOI.HSSF.Util;
+
+namespace InvoiceApp
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
